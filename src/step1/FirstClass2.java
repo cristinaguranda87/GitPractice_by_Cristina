c06956e42c9130f830ccb959806a65c4d2e5063f
cristinaguranda87 <114310339+cristinaguranda87@users.noreply.github.com>
2023-01-02 10:16:14 -0500

    Update FirstClass2.java

--- src/step1/FirstClass2.java
+++ src/step1/FirstClass2.java
@@ -3,5 +3,7 @@ package step1;
 public class FirstClass2 {
     public static void main(String[] args) {
         System.out.println("Hello Wooden spoon");
+         System.out.println("Hello Wooden spoon2");
+        
     }
 }
